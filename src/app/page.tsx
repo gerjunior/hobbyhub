efa9f9596ec404d0985a2828caea1376386593f9
Geraldo Silva <geraldojuni@hotmail.com>
2024-03-05 20:34:00 -0300

    feat: handle upvotes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,45 @@
 'use client';
+import { useState } from 'react';
 import PostCard from './post-card';
 import { usePosts } from './common/usePosts';
 
 export default function Home() {
+  const [orderBy, setOrderBy] = useState<'newest' | 'most_popular'>('newest');
   const { posts, isLoading } = usePosts();
+
+  const orderedPosts = posts?.sort((a, b) => {
+    if (orderBy === 'newest') {
+      return new Date(b.postedAt).getTime() - new Date(a.postedAt).getTime();
+    } else {
+      return b.upvotes - a.upvotes;
+    }
+  });
 
   return (
     <main className='flex min-h-screen flex-col items-center p-24'>
-      <h2 className='text-6xl font-bold mb-10'>Posts</h2>
+      <div className='flex gap-2 items-center w-full text-slate-500 mb-5 ml-10'>
+        <span>Order by: </span>
+        <button
+          className='p-2 bg-green-700 text-white rounded-lg'
+          onClick={() => setOrderBy('newest')}
+        >
+          Newest
+        </button>
+        <button
+          className='p-2 bg-cyan-600 text-white rounded-lg'
+          onClick={() => setOrderBy('most_popular')}
+        >
+          Most Popular
+        </button>
+      </div>
       <div className='w-full flex flex-col items-center gap-5'>
         {!isLoading &&
-          !!posts &&
-          posts.map((post) => {
+          !!orderedPosts &&
+          orderedPosts.map((post) => {
             return <PostCard key={post.id} post={post} />;
           })}
 
-        {!posts?.length && !isLoading && <div>No posts found</div>}
+        {!orderedPosts?.length && !isLoading && <div>No posts found</div>}
 
         {isLoading && <div>Loading...</div>}
       </div>
--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,4 +1,6 @@
+'use client';
 import Image from 'next/image';
+import useSWR, { mutate } from 'swr';
 import { AiOutlineLike } from 'react-icons/ai';
 import { Post } from '@/app/types';
 import { formatDate } from '@/app/helpers/date';
@@ -10,27 +12,49 @@ type PageProps = {
   };
 };
 
-const getPost = async (id: string) => {
-  try {
-    const response = await fetch(
-      process.env.NEXT_PUBLIC_API_URL + '/posts/' + id,
-    );
-    const data = await response.json();
-    return data as Post;
-  } catch (error) {
-    console.error(error);
-    return null;
-  }
+const getPost = async (url: string) => {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/${url}`);
+  const data = await response.json();
+  return data as Post;
 };
 
-export default async function Page({ params: { id } }: PageProps) {
-  const post = await getPost(id);
+export default function Page({ params: { id } }: PageProps) {
+  const { data: post, isLoading } = useSWR(`posts/${id}`, getPost);
+
+  if (isLoading) {
+    return <div className='h-screen w-full'>Loading...</div>;
+  }
 
   if (!post) {
     return <div className='h-screen w-full'>Post not found</div>;
   }
 
   const formattedDate = formatDate(post.postedAt);
+
+  const handleUpvote = async () => {
+    mutate(`posts/${id}`, { ...post, upvotes: (post.upvotes || 0) + 1 }, false);
+
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/posts/${id}`,
+        {
+          method: 'PATCH',
+          body: JSON.stringify({ upvotes: (post.upvotes || 0) + 1 }),
+        },
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+      } else {
+        console.error(response);
+        alert('Failed to upvote post');
+      }
+    } catch (error) {
+      console.error(error);
+      alert('Failed to upvote post');
+    }
+  };
 
   return (
     <div className='h-screen w-full p-20'>
@@ -41,7 +65,7 @@ export default async function Page({ params: { id } }: PageProps) {
         <Image src={post.imageUrl} alt={post.title} width={300} height={300} />
         <div className='flex flex-row justify-between items-center w-full'>
           <div className='flex flex-row gap-3 items-center mt-5'>
-            <AiOutlineLike size={20} />
+            <AiOutlineLike size={20} onClick={handleUpvote} />
             <p className='text-slate-600'>{post.upvotes || 0} upvotes</p>
           </div>
           <div className='flex flex-row gap-2'>
