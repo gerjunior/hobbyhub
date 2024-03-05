c3a99d5410b84b0916785d33f857e52efa60b8d0
Geraldo Silva <geraldojuni@hotmail.com>
2024-03-05 20:43:57 -0300

    feat: search posts by title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,10 @@ import { usePosts } from './common/usePosts';
 
 export default function Home() {
   const [orderBy, setOrderBy] = useState<'newest' | 'most_popular'>('newest');
-  const { posts, isLoading } = usePosts();
+  const [search, setSearch] = useState<string>('');
+  const { posts: originalPosts, isLoading } = usePosts();
 
-  const orderedPosts = posts?.sort((a, b) => {
+  const orderedPosts = originalPosts?.sort((a, b) => {
     if (orderBy === 'newest') {
       return new Date(b.postedAt).getTime() - new Date(a.postedAt).getTime();
     } else {
@@ -15,31 +16,44 @@ export default function Home() {
     }
   });
 
+  const posts = orderedPosts?.filter((post) =>
+    post.title.toLowerCase().includes(search.toLowerCase()),
+  );
+
   return (
     <main className='flex min-h-screen flex-col items-center p-24'>
-      <div className='flex gap-2 items-center w-full text-slate-500 mb-5 ml-10'>
-        <span>Order by: </span>
-        <button
-          className='p-2 bg-green-700 text-white rounded-lg'
-          onClick={() => setOrderBy('newest')}
-        >
-          Newest
-        </button>
-        <button
-          className='p-2 bg-cyan-600 text-white rounded-lg'
-          onClick={() => setOrderBy('most_popular')}
-        >
-          Most Popular
-        </button>
+      <div className='flex flex-row justify-between w-full mb-5 ml-10'>
+        <div className='flex gap-2 items-center w-full text-slate-500'>
+          <span>Order by: </span>
+          <button
+            className='p-2 bg-green-700 text-white rounded-lg'
+            onClick={() => setOrderBy('newest')}
+          >
+            Newest
+          </button>
+          <button
+            className='p-2 bg-cyan-600 text-white rounded-lg'
+            onClick={() => setOrderBy('most_popular')}
+          >
+            Most Popular
+          </button>
+        </div>
+        <input
+          type='text'
+          placeholder='Search...'
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          className='p-2 rounded-lg bg-gray-100 mr-10 w-96'
+        />
       </div>
       <div className='w-full flex flex-col items-center gap-5'>
         {!isLoading &&
-          !!orderedPosts &&
-          orderedPosts.map((post) => {
+          !!posts &&
+          posts.map((post) => {
             return <PostCard key={post.id} post={post} />;
           })}
 
-        {!orderedPosts?.length && !isLoading && <div>No posts found</div>}
+        {!posts?.length && !isLoading && <div>No posts found</div>}
 
         {isLoading && <div>Loading...</div>}
       </div>
